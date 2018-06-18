4617666e8ff6025e6c01eaefb7bd27c2c6b391a6
Julien Morot <jmorot@gmail.com>
2018-06-18 21:22:24 +0200

    Ensure package and config are not removed

--- manifests/updates.pp
+++ manifests/updates.pp
@@ -1,4 +1,5 @@
 class lnxhardening::updates {
+    package {'unattended-upgrades': ensure=>'installed' }
     file { '/etc/apt/apt.conf.d/01autoremove-kernels':
         ensure  => file,
         source => "puppet:///modules/lnxhardening/01autoremove-kernels",
@@ -16,6 +17,13 @@ class lnxhardening::updates {
     file { '/etc/apt/apt.conf.d/50unattended-upgrades':
         ensure  => file,
         source => "puppet:///modules/lnxhardening/50unattended-upgrades",
+        owner => "root",
+        group => "root",
+        mode => "0644",
+    }
+    file { '/etc/apt/apt.conf.d/20auto-upgrades':
+        ensure  => file,
+        source => "puppet:///modules/lnxhardening/20auto-upgrades",
         owner => "root",
         group => "root",
         mode => "0644",
